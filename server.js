7d074f1e5d7c09dfa2b3077c755905a9e4447b1d
1 <1>
2025-05-15 18:38:40 +0200

    Оновлення серверного коду

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-require('dotenv').config();
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 const PASSWORD = process.env.ADMIN_PASSWORD;
+
+// В ES-модулях __dirname немає, треба створити так:
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(express.static('public'));
